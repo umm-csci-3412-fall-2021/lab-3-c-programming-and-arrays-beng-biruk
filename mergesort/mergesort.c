61385563b84e8cdbb17b5bea7a29d4fa6972842a
Banana Observed <golds349@morris.umn.edu>
2021-09-30 10:43:28 -0500

    Create helper functions and minimal code skeleton

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,23 @@
 #include "mergesort.h"
 
-void mergesort(int, int*) {
+#include <math.h>
+
+int* subarray(int start, int end, int* input){
+  // Given an array and the indices of the start and end points,
+  // return an array containing the contents of the argument
+  // between those indices.
+  int size = (end - start) + 1; // +1 because the indices are both inclusive
+  int* output = (int*) malloc(size*sizeof(int));
+
+  return output;
+}
+
+void mergesort(int size, int* to_sort) {
+  if (size <= 1) {
+    return; // A single element or empty array is trivially/vacuously sorted
+  }
   // This obviously doesn't actually do any *sorting*, so there's
   // certainly work still to be done.
+  int midpoint = floor((size-1)/2);
   return;
 }
