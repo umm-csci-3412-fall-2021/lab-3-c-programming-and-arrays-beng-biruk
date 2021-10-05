1050016d8c59b4ff64848453d86758548e04e161
Banana Observed <golds349@morris.umn.edu>
2021-10-05 10:33:59 -0500

    Create skeleton for array_merge; actual logic not yet added, and valgrind not yet run.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,23 @@
 #include "array_merge.h"
 
+int find_least(int num_arrays, int* sizes, int* positions, int** values){
+
+}
+
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+
+  int* array_positions = (int*)calloc(num_arrays, sizeof(int)); //Sets contents to 0
+  int total_size = 0;
+  for(int i = 0; i < num_arrays; i++){
+    total_size += sizes[i];
+  }
+
+  int* output = calloc(total_size, int);
+  for(int i = 0; i < total_size; i++){
+    output[i] = find_least(num_arrays, sizes, array_positions, values); 
+    //find_least updates array_positions as a side effect
+  }
+
+  free(array_positions);
+  return output;
 }
